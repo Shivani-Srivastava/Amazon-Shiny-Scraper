e778eb68fc37584c39754df21f894bd186027876
Shivani Srivastava <shivani141296@gmail.com>
2022-03-25 09:32:56 +0530

    Update server.R

--- server.R
+++ server.R
@@ -1,3 +1,9 @@
+
+# Install / Load relevant packages
+if(!"pacman" %in% installed.packages()[,"Package"]) install.packages("pacman")
+pacman::p_load(rvest, dplyr, tidyr, stringr,shiny,markdown,DT)
+#pacman::p_load(DT)
+
 server <- shinyServer(function(input, output,session) {
   
   
